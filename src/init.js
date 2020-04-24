a08b6ea953d01e9c7f76351ddb385d9e52ecafdc
Jaskaran Singh <snak3codes@outlook.com>
2020-04-24 15:23:26 -0400

    Added rAF 📚benefits

--- src/init.js
+++ src/init.js
@@ -22,3 +22,9 @@ async function init() {
 }
 
 init();
+
+/* == Benefits of requestAnimationFrame == :
+- The browser can optimize it, so animations will be smoother 
+- Animations in inactive tabs will stop, allowing the CPU to chill
+- More battery-friendly
+*/
