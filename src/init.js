745916e46c97ea0a84f1b253b70cf102007f88db
Jaskaran Singh <snak3codes@outlook.com>
2020-04-24 15:32:43 -0400

    Started State 🔧

--- src/init.js
+++ src/init.js
@@ -1,8 +1,5 @@
+import gameState from "./gameState";
 const TICK_RATE = 3000; // Tick every 3 seconds
-
-function tick() {
-  console.log("tick", Date.now());
-}
 
 async function init() {
   console.log("Starting Game");
@@ -13,7 +10,7 @@ async function init() {
     const now = Date.now();
 
     if (nextTimeToTick <= now) {
-      tick();
+      gameState.tick();
       nextTimeToTick = now + TICK_RATE;
     }
     requestAnimationFrame(nextAnimationFrame);
